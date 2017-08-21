40f4ce38d141450927dff34a04aa3f37ba84f54d
tuayo77 <tuayoborel@yahoo.fr>
2017-08-21 20:18:40 +0200

    setup fr lang

---
+++ resources/views/widgets/footer.blade.php
@@ -0,0 +1,9 @@
+<div class="footer">
+    <div class="row">
+        <div class="col-sm-6">
+        </div>
+        <div class="col-sm-6 text-right">
+           myfriendser © 2017 Tous droits réservés.
+        </div>
+    </div>
+</div>
---
+++ resources/views/widgets/menu.blade.php
@@ -0,0 +1,28 @@
+<div class="menu">
+    <ul class="list-group">
+        <li class="list-group-item">
+            <a href="{{ url('/') }}" class="menu-home">
+                <i class="fa fa-home"></i>
+                Accueil
+            </a>
+        </li>
+        <li class="list-group-item">
+            <a href="{{ url('/nearby') }}" class="menu-nearby">
+                <i class="fa fa-map-marker"></i>
+                Proche
+            </a>
+        </li>
+        <li class="list-group-item">
+            <a href="{{ url('/groups') }}" class="menu-groups">
+                <i class="fa fa-users"></i>
+                Groupes
+            </a>
+        </li>
+        <li class="list-group-item">
+            <a href="{{ url('/direct-messages') }}" class="menu-dm">
+                <i class="fa fa-commenting"></i>
+                Messages directs
+            </a>
+        </li>
+    </ul>
+</div>
